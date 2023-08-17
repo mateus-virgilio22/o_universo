30ffd3e9791a41ecf70c577cb2949790347245b1
Mateus Virgílio <virgilionr15@gmail.com>
2023-08-17 08:53:00 -0300

    Integração do site com a API

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 // Variáveis e seleção de elementos 
-const planetaInput = document.querySelector("#planeta-input");
+const planetaInput = document.querySelector("#planetaInput");
 const searchBtn = document.querySelector("#search");
 
 const nameElement = document.querySelector("#nome");
 const ePlanetaElement = document.querySelector("#ePlaneta");
-const inclinacaoElement = document.querySelector("#inclinacao");
-const massaElement = document.querySelector("#massa");
+const translacaoElement = document.querySelector("#translacao");
+const rotacaoElement = document.querySelector("#rotacao");
 const gravidadeElement = document.querySelector("#gravidade");
 
 
 
 // Funções
+const getPlanetData = async(planeta) => {
+
+    const apiPlanetURL = `https://api.le-systeme-solaire.net/rest/bodies/${planeta}`;
+
+     const res = await fetch(apiPlanetURL);
+     const data = await res.json();
+
+     return data 
+}
+
+const showPlanetData = async(planeta) => {
 
 
+    const data = await getPlanetData(planeta);
+
+    
+
+
+    nameElement.innerText = data.englishName;
+    ePlanetaElement.innerText = data.bodyType;
+    translacaoElement.innerText = data.sideralOrbit;
+    rotacaoElement.innerText = data.sideralRotation;
+    gravidadeElement.innerText = data.gravity;
+    
+}
 
 // Eventos
 searchBtn.addEventListener("click", (e) => {
@@ -20,7 +43,5 @@ searchBtn.addEventListener("click", (e) => {
 
     const planeta = planetaInput.value;
 
-    console.log(planeta)
-
-    //showWeatherData(planeta);
+    showPlanetData(planeta);
 });
